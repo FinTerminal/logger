a180c5e2c0aa9852c33759993032f4e0b89c2175
Your Name <you@example.com>
2026-02-17 18:48:54 +0100

    impl: depth functional and function_name macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,20 @@
 use colored::Colorize;
 use std::fmt::Display;
 
+pub struct Logger {
+    depth: i32,
+}
+
+#[macro_export]
+macro_rules! function_name {
+    () => {{
+        fn f() {}
+        let name = std::any::type_name_of_val(&f);
+        name.strip_suffix("::f").unwrap_or(name);
+        name.split("::{{closure}}").next().unwrap_or(name)
+    }};
+}
+
 pub enum Type {
     Log,
     Warning,
@@ -17,11 +31,34 @@ impl Display for Type {
     }
 }
 
+impl Logger {
+    pub fn new(depth: i32) -> Logger {
+        return Logger { depth };
+    }
+
+    pub fn log(&self, function: &str, t: Type, content: impl std::fmt::Display) {
+        let function_arr = function.split("::").collect::<Vec<&str>>();
+        let function =
+            function_arr[((function_arr.len() - 1) - (self.depth as usize))..].join("::");
+
+        let output = match t {
+            Type::Error => format!("[{}]: {}", function, content).red(),
+            Type::Warning => format!("[{}]: {}", function, content).yellow(),
+            Type::Log => format!("[{}]: {}", function, content).white(),
+        };
+
+        println!("{}", output);
+    }
+}
+
 pub fn log(function: &str, t: Type, content: impl std::fmt::Display) {
+    let function_arr = function.split("::").collect::<Vec<&str>>();
+    let function = function_arr[((function_arr.len() - 1) - 1)..].join("::");
+
     let output = match t {
         Type::Error => format!("[{}]: {}", function, content).red(),
         Type::Warning => format!("[{}]: {}", function, content).yellow(),
-        Type::Log =>  format!("[{}]: {}", function, content).white(),
+        Type::Log => format!("[{}]: {}", function, content).white(),
     };
 
     println!("{}", output);
